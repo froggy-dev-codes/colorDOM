226a0221babc46bb2402493adb6f885133b2adf0
froggy-dev-codes <froggy.dev.codes@gmail.com>
2025-05-28 08:41:00 -0500

    modificando

--- js/rgb.js
+++ js/rgb.js
@@ -6,6 +6,8 @@ let inputAzul = document.getElementById("inputAzul");
 let textoRojo = document.getElementById("textoRojo");
 let textoVerde = document.getElementById("textoVerde");
 let textoAzul = document.getElementById("textoAzul");
+let colorTexto = document.getElementById("colorTexto");
+
 
 
 let rojo = inputRojo.value;
@@ -15,28 +17,31 @@ let azul = inputAzul.value;
 textoRojo.textContent = rojo;
 textoVerde.textContent = verde;
 textoAzul.textContent = azul;
+colorTexto.textContent = `${rojo}, ${verde}, ${azul}`
+
 
 
 // funcion actualizar color
 function actualizarColor (rojo, verde, azul){
     const colorRGB = `rgb(${rojo}, ${verde}, ${azul})`
     document.body.style.backgroundColor = colorRGB;
+    colorTexto.textContent = colorRGB; 
 }
 
 
 // para actualizar el colo r rojo, verde, azul
 inputRojo.addEventListener("change", (e)=> {
-    let rojo = e.target.value;
+    rojo = e.target.value;
     textoRojo.textContent = rojo;
     actualizarColor(rojo, verde, azul);
 })
 inputVerde.addEventListener("change", (e)=> {
-    let verde = e.target.value;
+    verde = e.target.value;
     textoVerde.textContent = verde;
     actualizarColor(rojo, verde, azul);
 })
 inputAzul.addEventListener("change", (e)=> {
-    let azul = e.target.value;
+    azul = e.target.value;
     textoAzul.textContent = azul
     actualizarColor(rojo, verde, azul);
 })
